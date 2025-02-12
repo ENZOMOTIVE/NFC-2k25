4c8c117ef0f724d6199be6ccf6dc7bce1f14f362
ENZOMOTIVE <aayushmanbhabapadhy@gmail.com>
2025-02-12 23:55:15 +0530

    Enzomotive

--- nfc-25/app/pages/Footer/footer.tsx
+++ nfc-25/app/pages/Footer/footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
     return (
       <footer className="w-full text-center py-4 bg-black text-white">
-        Made by Enzomotive with ♡
-      </footer>
+  Made by <a href="https://github.com/ENZOMOTIVE" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:underline">Enzomotive</a> with ♡
+</footer>
     );
   }
   
