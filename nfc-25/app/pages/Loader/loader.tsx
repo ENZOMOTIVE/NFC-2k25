6ad5c2868000347061da4bb9ade8e1f89aab71ab
ENZOMOTIVE <aayushmanbhabapadhy@gmail.com>
2025-02-12 23:39:05 +0530

    Bug fixes

--- nfc-25/app/pages/Loader/loader.tsx
+++ nfc-25/app/pages/Loader/loader.tsx
@@ -1,10 +1,11 @@
 "use client";
 import LoaderGif from '@/assets/loader.gif'
+import Image from 'next/image';
 
 const Loader = () => {
     return (
       <div className="fixed top-0 left-0 w-full h-screen bg-black flex items-center justify-center">
-        <img src={LoaderGif.src} alt="Loading..." width={150} height={150} />
+        <Image src={LoaderGif.src} alt="Loading..." width={150} height={150} />
       </div>
     );
   };
--- nfc-25/app/pages/Rules/page.tsx
+++ nfc-25/app/pages/Rules/page.tsx
@@ -3,8 +3,7 @@ import { Scroll } from "lucide-react"
 
 export default function Rules() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-red-900 via-red-800 to-gray-900
- text-white flex flex-col items-center py-10 px-4 relative overflow-hidden">
+    <div className="min-h-screen bg-gradient-to-br from-red-900 via-red-800 to-gray-900 text-white flex flex-col items-center py-10 px-4 relative overflow-hidden">
       {/* Background pattern - same as fixtures page */}
       <div className="absolute inset-0 bg-[url('data:image/svg+xml;charset=utf-8,%3Csvg xmlns=%22http://www.w3.org/2000/svg%22 width=%22100%22 height=%22100%22 viewBox=%220 0 100 100%22%3E%3Cpath d=%22M96 95h4v1h-4v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9zm-1 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9z%22 fill-opacity=%22.05%22 fill=%22%23fff%22/%3E%3C/svg%3E')] opacity-20"></div>
 
